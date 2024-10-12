64824be8856fd0b90729697517b6284919404553
bottlecom <116091293+bottlecom@users.noreply.github.com>
2024-10-12 22:59:56 +0300

    Add files via upload

---
+++ Pug-games/script.js
@@ -0,0 +1,11 @@
+document.getElementById("startGameBtn").addEventListener("click", function() {
+  alert("Гра починається! (Тут буде перехід на ігровий екран)");
+});
+
+document.getElementById("settingsBtn").addEventListener("click", function() {
+  alert("Налаштування поки що недоступні.");
+});
+
+document.getElementById("exitBtn").addEventListener("click", function() {
+  alert("Вихід з гри.");
+});
